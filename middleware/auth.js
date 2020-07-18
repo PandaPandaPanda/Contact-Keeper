489df724bf2d73533de973e15542f1b21de6cd19
Kevin <emailofkevinxu@gmail.com>
2020-07-17 23:41:20 -0400

    fix

--- middleware/auth.js
+++ middleware/auth.js
@@ -14,7 +14,7 @@ module.exports = function (req, res, next) {
   try {
     const decoded = jwt.verify(
       token,
-      config.get("jwtSecret") || process.env.jwtSecret
+      process.env.jwtSecret || config.get("jwtSecret")
     );
     req.user = decoded.user;
     next();
